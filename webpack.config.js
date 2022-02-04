1177d513a31ea200e88aa5159c55214984a18917
a8an7a <cp.291912@gmail.com>
2022-02-05 01:29:24 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import { CleanWebpackPlugin } from 'clean-webpack-plugin'
+import CopyWebpackPlugin from 'copy-webpack-plugin'
+
+export default {
+    context: resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: { main: './index.js' },
+    output: {
+        filename: '[name].js',
+        path: resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        alias: { '@': resolve(__dirname, 'src') },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: './index.html' }),
+        new MiniCssExtractPlugin(),
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: resolve(__dirname, 'src/favicon.png'),
+                    to: resolve(__dirname, 'dist'),
+                },
+            ],
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [['postcss-preset-env']],
+                            },
+                        },
+                    },
+                ],
+            },
+            { test: /\.ico$/i, type: 'asset/resource' },
+        ],
+    },
+}
